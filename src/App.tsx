9bb517beaaccda05686392710ec38e40e4f92da9
José Raposo <jose.brissos.raposo@gmail.com>
2024-02-05 01:33:42 +0000

    style button

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: FC = () => {
     <div className="flex flex-col items-center justify-center h-screen">
       <LoadingImage src={currentImage} className="w-96 h-64" />
       <button
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md"
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md transition duration-300 ease-in-out hover:bg-blue-400 active:bg-blue-600"
         onClick={toggleImage}
       >
         Load Another Image
